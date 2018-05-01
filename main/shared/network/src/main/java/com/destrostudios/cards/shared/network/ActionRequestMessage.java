8b94041c206790de4d7cfdc49ef33af1e47e276e
Etherblood <philipp@jobach.de>
2018-05-01 15:11:17 +0200

    frontend & backend can now send/receive actions via network

--- main/shared/network/src/main/java/com/destrostudios/cards/shared/network/ActionRequestMessage.java
+++ main/shared/network/src/main/java/com/destrostudios/cards/shared/network/ActionRequestMessage.java
@@ -2,13 +2,11 @@ package com.destrostudios.cards.shared.network;
 
 import com.destrostudios.cards.shared.events.Event;
 import com.jme3.network.AbstractMessage;
-import com.jme3.network.serializing.Serializable;
 
 /**
  *
  * @author Philipp
  */
-@Serializable
 public class ActionRequestMessage extends AbstractMessage {
 
     private Event action;
--- main/shared/network/src/main/java/com/destrostudios/cards/shared/network/SerializerSetup.java
+++ main/shared/network/src/main/java/com/destrostudios/cards/shared/network/SerializerSetup.java
@@ -1,5 +1,6 @@
 package com.destrostudios.cards.shared.network;
 
+import com.destrostudios.cards.shared.rules.game.GameStartEvent;
 import com.jme3.network.serializing.Serializer;
 
 /**
@@ -9,8 +10,17 @@ import com.jme3.network.serializing.Serializer;
 public class SerializerSetup {
 
     static {
-        Serializer.registerClass(ActionNotificationMessage.class);
-        Serializer.registerClass(ActionRequestMessage.class);
+        registerClasses(
+                ActionNotificationMessage.class,
+                ActionRequestMessage.class,
+                GameStartEvent.class);
+    }
+
+    private static void registerClasses(Class<?>... classes) {
+        for (Class<?> classe : classes) {
+            //registers the class without need for @Serializable
+            Serializer.getSerializerRegistration(classe, false);
+        }
     }
 
     public static void ensureInitialized() {

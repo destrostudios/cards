89e06a6a2c31ef8842da7efbde6adec19a19eb54
Gerd <gerdemmanuel@gmail.com>
2018-05-03 21:47:35 +0200

    Components umbenannt

--- main/shared/rules/src/main/java/com/destrostudios/cards/shared/rules/Components.java
+++ main/shared/rules/src/main/java/com/destrostudios/cards/shared/rules/Components.java
@@ -21,7 +21,8 @@ public class Components {
     public static final ComponentDefinition<Integer> HAND_CARDS = new ComponentDefinition<>("hand");
     public static final ComponentDefinition<Integer> HEALTH = new ComponentDefinition<>("health");
     public static final ComponentDefinition<Integer> LIBRARY = new ComponentDefinition<>("library");
-    public static final ComponentDefinition<Integer> MANA_ZONE = new ComponentDefinition<>("manaZone");
+    public static final ComponentDefinition<Integer> GRAVEYARD = new ComponentDefinition<>("graveyard");
+    public static final ComponentDefinition<Integer> LAND_ZONE = new ComponentDefinition<>("landZone");
     public static final ComponentDefinition<Integer> NEXT_PLAYER = new ComponentDefinition<>("nextPlayer");
     public static final ComponentDefinition<Integer> OWNED_BY = new ComponentDefinition<>("ownedBy");
     public static final ComponentDefinition<Void> SPELL_CARD = new ComponentDefinition<>("spellCard");

66b78b0a8bd96a57a6f37c87b96903393e5ba04d
carlkuesters <carl.kuesters@youengineering.com>
2023-03-31 22:59:33 +0200

    Added more card tests.

---
+++ test/src/test/java/com/destrostudios/cards/shared/rules/spells/general/TestSimpleTargetStatsBuff.java
@@ -0,0 +1,19 @@
+package com.destrostudios.cards.shared.rules.spells.general;
+
+import com.destrostudios.cards.shared.rules.Components;
+import com.destrostudios.cards.shared.rules.TestGame;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class TestSimpleTargetStatsBuff extends TestGame {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/spells/simple_target_stats_buff.csv", numLinesToSkip = 1)
+    public void testBuffOnCast(String template, int bonusAttack, int bonusHealth) {
+        int target = createVanilla(0, 1, 1, player, Components.CREATURE_ZONE);
+        int card = create(template, player, Components.HAND);
+        castFromHand(card, target);
+        assertAttack(target, 1 + bonusAttack);
+        assertHealth(target, 1 + bonusHealth);
+    }
+}

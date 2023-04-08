ad0900f0d56ac1189ab00fc926ba8ffae90a7fc8
carlkuesters <carl.kuesters@youengineering.com>
2023-04-08 03:40:46 +0200

    Rename Ultriia card.

--- test/src/test/java/com/destrostudios/cards/shared/rules/creatures/specific/TestUltriiaFlowerOfCorruption.java
+++ test/src/test/java/com/destrostudios/cards/shared/rules/creatures/specific/TestUltriiaFlowerOfCorruption.java
@@ -4,11 +4,11 @@ import com.destrostudios.cards.shared.rules.Components;
 import com.destrostudios.cards.shared.rules.TestGame;
 import org.junit.jupiter.api.Test;
 
-public class TestUltriiaTheFlowerOfCorruption extends TestGame {
+public class TestUltriiaFlowerOfCorruption extends TestGame {
 
     @Test
     public void testReduceManaCostOnPlayerDamage() {
-        int card = create("creatures/ultriia_the_flower_of_corruption", player, Components.HAND);
+        int card = create("creatures/ultriia_flower_of_corruption", player, Components.HAND);
         damage(opponent, 1);
         assertManaCost(card, 7);
     }

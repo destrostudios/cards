ac2ffea1e7f7000f36db170c50c40168a63da2be
carlkuesters <carl.kuesters@gmail.com>
2023-04-29 01:27:04 +0200

    First balancing round.

--- test/src/test/java/com/destrostudios/cards/test/creatures/specific/TestHighPriestessLlyncis.java
+++ test/src/test/java/com/destrostudios/cards/test/creatures/specific/TestHighPriestessLlyncis.java
@@ -9,7 +9,7 @@ public class TestHighPriestessLlyncis extends TestGame {
 
     @Test
     public void testHealDamagedCharacterOnSummon() {
-        damage(player, 8);
+        damage(player, 6);
         int card = create("creatures/high_priestess_llyncis", player, Components.Zone.HAND);
         castFromHand(card, player);
         assertHealthAndDamaged(player, GameConstants.PLAYER_HEALTH - 1);
@@ -19,6 +19,6 @@ public class TestHighPriestessLlyncis extends TestGame {
     public void testDamageUndamagedCharacterOnSummon() {
         int card = create("creatures/high_priestess_llyncis", player, Components.Zone.HAND);
         castFromHand(card, opponent);
-        assertHealthAndDamaged(opponent, GameConstants.PLAYER_HEALTH - 7);
+        assertHealthAndDamaged(opponent, GameConstants.PLAYER_HEALTH - 5);
     }
 }
